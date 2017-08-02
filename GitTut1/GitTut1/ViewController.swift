f0983ca2c44f0c10f7393109969d4279063ef37f
Tan Le Quang <tanleq94@gmail.com>
2017-08-02 13:10:40 +0200

    Start

---
+++ GitTut1/GitTut1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitTut1
+//
+//  Created by Tan Le Quang on 02/08/2017.
+//  Copyright © 2017 Tan Le Quang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
